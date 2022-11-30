be423c22d55d9aa359084f2126d7b80e405f85bc
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-11-30 11:21:31 +0700

    Directive 2

--- src/app/components/directive/directive.component.ts
+++ src/app/components/directive/directive.component.ts
@@ -1,20 +1,42 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-directive',
   templateUrl: './directive.component.html',
   styleUrls: ['./directive.component.css'],
 })
-export class DirectiveComponent implements OnInit {
+export class DirectiveComponent implements OnInit, OnChanges {
   @Input() Mycolor!: string;
   @Input() Mynumber!: Number;
   @Input() Mywidth!: String;
   @Input() Myheight!: String;
+  @ViewChild('closebutton') closebutton!: any;
+  @Output() countChanged: EventEmitter<any> = new EventEmitter();
 
   constructor() {}
-
-  ngOnInit(): void {
-    // this.test();
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (!changes.Mycolor.firstChange) {
+      alert('First change: ');
+    }
   }
-  test() {}
+  ngOnInit(): void {}
+  onSubmit() {
+    this.countChanged.emit([
+      this.Mycolor,
+      this.Mynumber,
+      this.Mywidth,
+      this.Myheight,
+    ]);
+    this.closebutton.nativeElement.click(); // đóng modal
+  }
 }
--- src/app/components/test1/test1.component.ts
+++ src/app/components/test1/test1.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-test1',
   templateUrl: './test1.component.html',
   styleUrls: ['./test1.component.css'],
 })
-export class Test1Component implements OnInit {
+export class Test1Component implements OnInit, OnChanges {
   Color: string = 'red';
   Width: String = '100px';
   Height: String = '100px';
   Number: Number = 10;
   constructor() {}
-
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
   ngOnInit(): void {}
+  countChangedHandler(data: string): void {
+    console.log(data);
+    console.log(data[0]);
+    this.Color = data[0];
+    this.Number = Number(data[1]);
+    this.Width = data[2];
+    this.Height = data[3];
+  }
 }
