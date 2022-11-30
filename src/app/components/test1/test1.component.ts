3968f1c1f668877bb2632ba0c31ae8cc4f86179e
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-11-30 16:36:48 +0700

    edit div 7

--- src/app/components/test1/test1.component.ts
+++ src/app/components/test1/test1.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-test1',
@@ -10,6 +16,11 @@ export class Test1Component implements OnInit, OnChanges {
   Width: String = '100px';
   Height: String = '100px';
   Number: number = 10;
+  // Color!: string ;
+  // Width!: String ;
+  // Height!: String;
+  // Number!: number ;
+  @ViewChild('closebutton') closebutton!: any;
   constructor() {}
   ngOnChanges(changes: SimpleChanges): void {}
   ngOnInit(): void {}
@@ -19,4 +30,8 @@ export class Test1Component implements OnInit, OnChanges {
     this.Width = data[2];
     this.Height = data[3];
   }
+  add() {
+    this.closebutton.nativeElement.click();
+  }
+  onSubmit() {}
 }
